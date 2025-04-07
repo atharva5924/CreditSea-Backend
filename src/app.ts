3754e85257bbd44f54428df7b33a6ee1038313ec
atharva5924 <atharvanile2005@gmail.com>
2025-04-07 15:45:38 +0530

    frontend url integarted

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "https://credit-sea-loan-potal-frontend-u2qg.vercel.app/", 
     credentials: true, 
   }));
 app.use(express.json());
